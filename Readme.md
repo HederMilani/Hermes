eac97c9831ef695df7acdbab4b5be66e962b0cc6
Heder Milani <80220353+HederMilani@users.noreply.github.com>
2023-03-04 14:23:36 -0300

    ADD - configured from eureka server and build image

---
+++ Readme.md
@@ -0,0 +1,12 @@
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)
+
+# Hermes
+
+Microserviço para Discovery Server e Load Balance no sistema **GeekInvesting**.
+
+# Release
+
+
+## 0.0.1-SNAPSHOT
+
+Versão compõe a criação e configuração do servidor para receber conexão dos clients Eureka.
--- src/main/java/com/api/hermes/HermesApplication.java
+++ src/main/java/com/api/hermes/HermesApplication.java
@@ -2,8 +2,10 @@ package com.api.hermes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class HermesApplication {
 
 	public static void main(String[] args) {
